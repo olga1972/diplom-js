16c22bc7c208eb3fd651f47014cc2a83c4f7c4d9
Olga <olga.sadyreva@mail.ru>
2020-08-26 18:38:05 +0500

    token undefined

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,84 +1,96 @@
 import React, {Component} from 'react';
-import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+//import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 
 import Header from '../../components/Header/Header';
 import PhotosList from '../../components/PhotosList/PhotosList';
 import store from '../../store';
-import Unsplash, {toJson, setBearerToken} from 'unsplash-js';
+import Unsplash, {toJson} from 'unsplash-js';
 
 
-  console.log('auth');
-    const unsplash = new Unsplash({
-        accessKey: "rOCoQmwn-eMbE_b57Bgd9LlZmpYTmTlp8dyQ8i3tBds",
-        secretKey: "vO_eYWwcjZtP10-Hk533hzyR9ZMsOESWTft6j1_i_Tc",
-        callbackUrl: "urn:ietf:wg:oauth:2.0:oob",
-      });
+// Создаем экземпляр объекта для доступа к API
+  const unsplash = new Unsplash({
+    accessKey: "rOCoQmwn-eMbE_b57Bgd9LlZmpYTmTlp8dyQ8i3tBds",
+    secretKey: "vO_eYWwcjZtP10-Hk533hzyR9ZMsOESWTft6j1_i_Tc",
+    callbackUrl: "http://localhost:3000/auth",
+  });
 
-      const authenticationUrl = unsplash.auth.getAuthenticationUrl([
-        "public",
-        "write_likes",
-      ]);
+// Генерируем адрес страницы аутентификации на unsplash.com
+// и указываем требуемые разрешения (permissions)
+  const authenticationUrl = unsplash.auth.getAuthenticationUrl([
+    "public",
+    "write_likes",
+  ]);
 
-      console.log('url '+ authenticationUrl);
 
-      window.location.assign(authenticationUrl);
+  function handleClick (){
+    window.location.assign(authenticationUrl);
+  }
+
+// Считываем GET-параметр code из URL
+// www.example.com/auth?code=abcdef123456...
+  const code = window.location.search.split("code=")[1];
+  console.log(code);
+
+// Если код передан, отправляем запрос на получение токена
+  if (code) {
+    unsplash.auth.userAuthentication(code)
+    .then(res => {
+      console.dir(res);
+      res.json();
+    })
+    .then(json => {
+      // Сохраняем полученный токен
+      console.log(json.access_token)
+      localStorage.setItem('token', json.access_token)
+      unsplash.auth.setBearerToken(json.access_token);
       
-      // тут возможно нужна проверка на корректность открытого url
-      const code = window.location.search.split("code=")[1];
-      console.log('code' + code);
-      
-      if (code) {
-        unsplash.auth.userAuthentication(code);
-        unsplash.auth.setBearerToken(toJson.access_token);
-        console.log(toJson.access_token);
-        unsplash.auth.setBearerToken(toJson.access_token);
-        console.log(toJson.access_token);
-      
-        unsplash.photos.listPhotos(2, 15, "latest")
-        .then(res =>
-            res.json())
-        .then(json => {
-          console.log('json' + json);})
-        .catch(err => console.log('Auth err', err));
-        
-        //unsplash.photos.likePhoto("kBJEJqWNtNY");
-      }
+      // Теперь можно сделать что-то от имени пользователя
+      // Например, поставить лайк фотографии
+      unsplash.photos.likePhoto("mtNweauBsMQ");
+      /* unsplash.photos.getPhoto("mtNweauBsMQ")
+                  .then(toJson)
+                  .then(json => {
+                    console.log(json);
+                  }); */
+    })
+    .catch(err => console.log('Auth err', err));
+  }
 
+  //получаем фото, грузим их.
+  const  listPhoto = (start, end, access_token) => {
 
+    unsplash.auth.setBearerToken(access_token);
 
- class App extends Component {
-    componentDidMount() {
-      fetch('https://source.unsplash.com/daily')
-        .then(res => res.json())
-        .then(data => {
-            //this.setState({ photos: data });
-            console.log(data);
-        })
-        .catch(err => {
-            console.log('Error!', err);
-        });
-    }
+    return unsplash.photos.listPhotos(start, end, "latest")
+      .then(res => res.json());
+  };
+       
+  function loadPhotos() {
+    let start = 1;
+    let end = 10;
+
+    const data = listPhoto(+start, +end, localStorage.getItem('token'))
+    data.then(d => console.log(d));
+  }
+
+  class App extends Component {
+    
 
   render() {
     const isAuth =true;
     return (
-
-      isAuth ? (
-        <div className="wrapper">
-                <Header></ Header>
-
-                <PhotosList />
-            </div>
-
-      ) : (
-        <Redirect to="/" />
-      )
+      <div className="wrapper">
+          <Header></ Header>
+          <button onClick={handleClick}>Авторизация</button>
+          <button onClick={loadPhotos}>Загрузить фото</button>
+          <PhotosList />
+      </div>
     )
   }
-
 }
 
- export default App;
+export default App;
 
 
 
